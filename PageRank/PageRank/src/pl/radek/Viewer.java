8eb757bc7aafdf7783ac7cf03cf02f718b95f922
rrydziu <rrydziu@e21eebde-923b-0410-9b4e-817778c5b05e>
2008-02-22 23:36:25 +0000

    Initial import.

---
+++ PageRank/PageRank/src/pl/radek/Viewer.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package pl.radek;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Radek
+ *
+ */
+public class Viewer extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
