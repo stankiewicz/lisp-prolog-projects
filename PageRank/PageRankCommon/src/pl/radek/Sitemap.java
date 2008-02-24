d65a3cf8932a171553e1c5594b50ec435432d333
rrydziu <rrydziu@e21eebde-923b-0410-9b4e-817778c5b05e>
2008-02-24 15:27:21 +0000

    Initial import.

---
+++ PageRank/PageRankCommon/src/pl/radek/Sitemap.java
@@ -0,0 +1,19 @@
+package pl.radek;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Sitemap {
+	public static void save(List<Node> nodes, OutputStream stream) {
+		
+	}
+	
+	public static List<Node> restore(InputStream stream){
+		ArrayList<Node> list = new ArrayList<Node>();
+		return list;
+	}
+}
